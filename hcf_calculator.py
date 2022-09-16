77a2c16570bed2805953fa35e86277d8d210a475
DaviesHub <davieschuks.a@gmail.com>
2022-09-16 16:14:23 +0200

    Put the calculator in a function

--- hcf_calculator.py
+++ hcf_calculator.py
@@ -1,32 +1,41 @@
-# Code to compute the highest common factor of two integers
-print("\nEnter any 2 positive integers")
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+# Function to compute the highest common factor of two integers
 
-while True:
-    if num1 <= 0 or num2 <= 0:
-        print("\nYou have entered a negative number. Enter 2 positive integers")
-        num1 = int(input("Enter the first number: "))
-        num2 = int(input("Enter the second number: "))
-        continue
-    elif num1 == num2:
-        hcf = num1 # HCF of the same numbers is the number
-        break
-    elif num1 == 1 or num2 == 1:
-        hcf = 1 # HCF of 1 and a positive number is 1
-        break
-    else:
-        if num1 < num2: # Find the smallest number
-            min_num = num1
-            max_num = num2
+def hcf_calculator(num1, num2):
+    """ 
+    This function finds the hcf of two numbers.
+
+    It receives two positive integres as inputs and returns an integer as output.
+    This fuction also prints out the hcf.
+    
+    """
+    while True:
+        if num1 <= 0 or num2 <= 0:
+            print("\nYou have entered a negative number. Enter 2 positive integers")
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            continue
+        elif num1 == num2:
+            hcf = num1 # HCF of the same numbers is the number
+            break
+        elif num1 == 1 or num2 == 1:
+            hcf = 1 # HCF of 1 and a positive number is 1
+            break
         else:
-            min_num = num2
-            max_num = num1
+            if num1 < num2: # Find the smallest number
+                min_num = num1
+                max_num = num2
+            else:
+                min_num = num2
+                max_num = num1
         
-        for i in range(1, min_num + 1):
-            if min_num % i == 0 and max_num % i == 0:
-                hcf = i # Update hcf
-        break
+            for i in range(1, min_num + 1):
+                if min_num % i == 0 and max_num % i == 0:
+                    hcf = i # Update hcf
+            break
 
+    
+    print("The highest common factor of {} and {} is {}".format(num1, num2, hcf))
 
-print("The highest common factor of {} and {} is {}".format(num1, num2, hcf))
+    return hcf
+
+hcf_calculator(120, 96)
