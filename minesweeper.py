a7a8c356d8dff639fa1e361840ec4bc6171621be
DaviesHub <davieschuks.a@gmail.com>
2022-10-12 21:25:31 +0200

    Count mines for corner dashes

--- minesweeper.py
+++ minesweeper.py
@@ -9,12 +9,12 @@ def generate_grid():
 def minesweeper(grid):
     """A simulation of minesweeper game"""
 
-    num_rows = 5
-    num_cols = 5
+    num_rows = len(grid)
+    num_cols = len(grid)
 
     output_grid = [["#"] * num_cols for null in range(num_rows)] # Initialize output grid
-    for i in range(1, 4): # Code for interior values
-        for j in range(1, 4):
+    for i in range(1, len(grid)): # Code for interior values
+        for j in range(1, len(grid)):
             mine_count = 0
             if grid[i][j] == "-":
                 # Upward check
@@ -46,9 +46,68 @@ def minesweeper(grid):
                     mine_count += 1 # left-center check
 
                 output_grid[i][j] = str(mine_count)
+            
+            else:
+                output_grid[i][j] = "#"
 
-    # Code for border values
-    # First row
+    # == Code for corner values ==
+    # Top left corner
+    if grid[0][0] == "-":
+        mine_count = 0
+        if grid[0][1] == "#":
+            mine_count += 1 # Adjacent right
+
+        if grid[1][1] == "#":
+            mine_count += 1 # Adjacent diagonal
+
+        if grid[1][0] == "#":
+            mine_count += 1 # Adjacent bottom
+
+        output_grid[0][0] = mine_count
+
+    # Top right corner
+    if grid[0][-1] == "-":
+        mine_count = 0
+        if grid[0][-2] == "#":
+            mine_count += 1 # Adjacent left
+
+        if grid[1][-2] == "#":
+            mine_count += 1 # Adjacent diagonal
+
+        if grid[1][-1] == "#":
+            mine_count += 1 # Adjacent bottom
+
+        output_grid[0][-1] = mine_count
+
+    # Bottom left corner
+    if grid[-1][0] == "-":
+        mine_count = 0
+        if grid[-1][1] == "#":
+            mine_count += 1 # Adjacent right
+
+        if grid[-1][1] == "#":
+            mine_count += 1 # Adjacent diagonal
+
+        if grid[-2][0] == "#":
+            mine_count += 1 # Adjacent top
+
+        output_grid[-1][0] = mine_count
+
+    # Bottom right corner
+    if grid[-1][-1] == "-":
+        mine_count = 0
+        if grid[-1][-2] == "#":
+            mine_count += 1 # Adjacent left
+
+        if grid[-2][-2] == "#":
+            mine_count += 1 # Adjacent diagonal
+
+        if grid[-2][-1] == "#":
+            mine_count += 1 # Adjacent top
+
+        output_grid[-1][-1] = mine_count
+
+    # == Code for interior border values ==
     
 
 
