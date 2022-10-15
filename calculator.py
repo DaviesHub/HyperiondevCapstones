fee0b43476b9a7ca069efb24302bdec0233ba066
DaviesHub <davieschuks.a@gmail.com>
2022-10-15 19:46:58 +0200

    Add comments

--- calculator.py
+++ calculator.py
@@ -1,15 +1,17 @@
 # A simple calculator program that performs arithmetic operations on two numbers
 
 
+# Define calculator function to be called when user opts for the calculator
 def calculator():
     """This function is a simple simulation of a binary operations calculator"""
 
+    # Prompt input for both numbers
     while True:
         num_one = input("Enter the first number: ")
         num_two = input("Enter the second number: ")
 
         try:
-            if "." in num_one:
+            if "." in num_one: # If one of the numbers has a point, it is considered to be a float. Otherwise, it is treated as an integer
                 num_one = float(num_one)
             else:
                 num_one = int(num_one)
@@ -26,6 +28,7 @@ def calculator():
 
     result = 0
 
+    # Prompt user for artithmetic operation
     while True:
         operation = input('''Enter the operation you'd like to perform on both numbers:
     # Enter + for addition
@@ -36,7 +39,8 @@ def calculator():
 
         if operation == "+":
             result = num_one + num_two
-            if isinstance(num_one, float) or isinstance(num_two, float):
+            # When one of the operands is a float, result is a decimal and is rounded to 2 decimal places
+            if isinstance(num_one, float) or isinstance(num_two, float): 
                 result = round(result, 2)
             break
     
