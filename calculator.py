ccc9cc67bc3ea1c2937a7a0e651b3462aef17847
DaviesHub <davieschuks.a@gmail.com>
2022-10-15 14:16:26 +0200

    Avoid division by zero if operation is division

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# A simple calculator program that performs binary operations on two numbers
+# A simple calculator program that performs arithmetic operations on two numbers
 
 while True:
     print("\n=== This calculator performs arithmetic operations on two numbers===\n")
@@ -17,11 +17,13 @@ while True:
         else:
             num_two = int(num_two)
             break
+
     except ValueError:
         print("Unsupported operand. Both operands must be numbers. Try again.")
 
 fhand = open("equations.txt", "a")
 result = 0
+
 while True:
     operation = input('''Enter the operation you'd like to perform on both numbers:
 # Enter + for addition
@@ -32,15 +34,27 @@ while True:
 
     if operation == "+":
         result = num_one + num_two
+        if isinstance(num_one, float) or isinstance(num_two, float):
+            result = round(result, 2)
     
     elif operation == "-":
         result = num_one - num_two
+        if isinstance(num_one, float) or isinstance(num_two, float):
+            result = round(result, 2)
 
     elif operation == "*":
         result = num_one * num_two
-    
+        if isinstance(num_one, float) or isinstance(num_two, float):
+            result = round(result, 2)
+
     elif operation == "/":
+        if num_two == 0:
+            print("Cannot perform division by zero")
+            break
+
         result = num_one / num_two
+        if isinstance(num_one, float) or isinstance(num_two, float):
+            result = round(result, 2)
 
 
 fhand.write(f"{num_one} {operation} {num_two} = {result}\n")
